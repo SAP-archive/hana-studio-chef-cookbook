29a8d8ce924d629301a004a52d9146afa7238dff
Dan-Joe Lopez <Dan-Joe.Lopez@sap.com>
2020-12-16 22:14:31 +0000

    update versioning

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,10 @@ maintainer       'SAP'
 maintainer_email 'Dan-Joe.Lopez@sap.com'
 license          'Apache 2.0'
 description      'Installs/Configures SAP HANA Studio'
-source_url       'https://github.com/sap/hana-studio-chef-cookbook' if respond_to?(:source_url)
-issues_url       'https://github.com/sap/hana-studio-chef-cookbook/issues' if respond_to?(:issues_url)
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+source_url       'https://github.com/sap/hana-studio-chef-cookbook'
+issues_url       'https://github.com/sap/hana-studio-chef-cookbook/issues'
+version          '2.0.0'
 
-depends 'windows', '~>1.0'
+depends 'windows'# , '~>1.0'
 
 supports 'windows'
